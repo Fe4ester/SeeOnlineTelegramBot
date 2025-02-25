611a3a8239088c7c5e8f87768dbad9f86d16aca9
Fe4ester <xdxdxd4305@gmail.com>
2025-02-26 00:50:26 +0200

    Добавление дополнительного меню, и реакции на кнопки

--- src/bot/handlers/system/help.py
+++ src/bot/handlers/system/help.py
@@ -3,11 +3,14 @@ from aiogram.types import Message
 from aiogram.filters import Command
 
 # Тексты
-from src.bot.texts.system_texts import help_command_text
+from src.bot.texts.system_texts import help_text
 
 router = Router()
 
 
 @router.message(Command('help'))
 async def cmd_help(message: Message):
-    await message.answer(help_command_text)
+    await message.answer(
+        help_text,
+        parse_mode='HTML'
+    )
--- src/bot/handlers/system/start.py
+++ src/bot/handlers/system/start.py
@@ -3,11 +3,18 @@ from aiogram.types import Message
 from aiogram.filters import Command
 
 # Тексты
-from src.bot.texts.system_texts import start_command_text
+from src.bot.texts.system_texts import start_text
+
+# Клавиатуры
+from src.bot.keyboards.reply import get_main_keyboard
 
 router = Router()
 
 
 @router.message(Command('start'))
 async def cmd_start(message: Message):
-    await message.answer(start_command_text)
+    await message.answer(
+        start_text,
+        parse_mode='HTML',
+        reply_markup=get_main_keyboard()
+    )
