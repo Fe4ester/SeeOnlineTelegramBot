fae6f64b6638d179e919da21b20be1be379b09fa
Fe4ester <xdxdxd4305@gmail.com>
2025-03-05 23:28:56 +0200

    Добавил переменную окружения PRIVATE и сделал проверку в middleware

--- src/bot/middlewares/whitelist_middleware.py
+++ src/bot/middlewares/whitelist_middleware.py
@@ -2,6 +2,8 @@ from aiogram import BaseMiddleware
 from aiogram.types import Message, CallbackQuery, TelegramObject
 from typing import Callable, Dict, Any, Awaitable
 
+from src.config.settings import settings
+
 
 class WhitelistMiddleware(BaseMiddleware):
     def __init__(self, allowed_users: list[int]):
@@ -14,14 +16,17 @@ class WhitelistMiddleware(BaseMiddleware):
             event: TelegramObject,
             data: Dict[str, Any]
     ) -> Any:
-        user_id = None
+        if settings.PRIVATE:
+            user_id = None
 
-        if isinstance(event, Message):
-            user_id = event.from_user.id
-        elif isinstance(event, CallbackQuery):
-            user_id = event.from_user.id
+            if isinstance(event, Message):
+                user_id = event.from_user.id
+            elif isinstance(event, CallbackQuery):
+                user_id = event.from_user.id
 
-        if user_id and user_id not in self.allowed_users:
-            return
+            if user_id and user_id not in self.allowed_users:
+                return
 
-        return await handler(event, data)
+            return await handler(event, data)
+        else:
+            return await handler(event, data)
--- src/config/settings.py
+++ src/config/settings.py
@@ -10,6 +10,7 @@ class Settings(BaseSettings):
     BOT_TOKEN: str
     EXTERNAL_SERVICE_API_URL: str
     REDIS_URL: str
+    PRIVATE: bool
     BOT_WHITELIST: list[int]
 
     model_config = SettingsConfigDict(env_file=ENV_FILE, env_file_encoding="utf-8")
